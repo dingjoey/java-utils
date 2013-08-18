ee966be6f6ce93e22dcdab0a9436232cd2971659
qiaoyi.dingqy <qiaoyi.dingqy@alibaba-inc.com>
2013-08-18 15:45:06 +0800

    code4fun wordcount  version 0.2

--- src/main/java/com/taobao/joey/wordcnt/WC2.java
+++ src/main/java/com/taobao/joey/wordcnt/WC2.java
@@ -45,6 +45,10 @@ public class WC2 {
     }
 
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+        if (args.length > 0) {
+            wordCntThreadsNum = Integer.parseInt(args[0]);
+        }
+
         long startReadFile = System.currentTimeMillis();
         mmapRead();
         long endReadFile = System.currentTimeMillis();
