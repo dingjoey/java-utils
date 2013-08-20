9c481ac149325feb96e2e00870d18be7f83c8e7f
qiaoyi.dingqy <qiaoyi.dingqy@alibaba-inc.com>
2013-08-20 22:11:00 +0800

    code4fun wordcount  version 1.0 final

--- src/main/java/com/taobao/joey/wordcnt/WC10.java
+++ src/main/java/com/taobao/joey/wordcnt/WC10.java
@@ -6,8 +6,7 @@ import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -21,6 +20,8 @@ import java.util.concurrent.CountDownLatch;
 public class WC10 {
     static byte[] punctuation
             = {':', '.', ',', ';', '-', '~', '?', '!', '\'', '\"', '\r', '\n', '\t', ' '};
+    static HashSet<String> stopWords
+            = new HashSet<String>(Arrays.asList("the", "and", "i", "to", "of", "a", "in", "was", "that", "had", "he", "you", "his", "my", "it", "as", "with", "her", "for", "on"));
     //
     static FileChannel fileChannel;
     static File file;
@@ -29,12 +30,16 @@ public class WC10 {
     static MappedByteBuffer input;
     //
     static int emmitTaskRunnableNum = 4;
-    static ArrayList<HashMap<String, MutableInt>> emitTaskOutput = new ArrayList<HashMap<String, MutableInt>>(emmitTaskRunnableNum);
+    static ArrayList<ArrayList<HashMap<String, MutableInt>>> emitTaskOutput = new ArrayList<ArrayList<HashMap<String, MutableInt>>>(emmitTaskRunnableNum);
     static CountDownLatch emmitTaskBarrier = new CountDownLatch(emmitTaskRunnableNum);
+    //
+    static int rankTaskRunnableNum = 4;
+    static WCResult[][] rankTaskOutput = new WCResult[rankTaskRunnableNum][];
+    static CountDownLatch rankTaskBarrier = new CountDownLatch(rankTaskRunnableNum);
 
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
         // init
-        init(4);
+        init(4, 4);
 
         // emmit task
         long startEmitTask = System.currentTimeMillis();
@@ -47,11 +52,24 @@ public class WC10 {
         long endEmitTask = System.currentTimeMillis();
         System.out.println("emit task consumes : " + (endEmitTask - startEmitTask));
 
+
+        rankTaskBarrier.await();
+        long startRankTask = System.currentTimeMillis();
+        Thread[] threads2 = new Thread[emmitTaskRunnableNum];
+        for (int i = 0; i < emmitTaskRunnableNum; i++) {
+            threads2[i] = new Thread(new RankTaskRunnable(i));
+            threads2[i].start();
+        }
+        rankTaskBarrier.await();
+        long endRankTask = System.currentTimeMillis();
+        System.out.println("emit task consumes : " + (endRankTask - startRankTask));
         //
+
+        merge();
 
     }
 
-    public static void init(int en) throws URISyntaxException, IOException {
+    public static void init(int en, int rn) throws URISyntaxException, IOException {
         // init emmit task thread num
         emmitTaskRunnableNum = en;
         // init emmit task input
@@ -59,12 +77,51 @@ public class WC10 {
         fileChannel = new RandomAccessFile(file, "rw").getChannel();
         fileLength = (int) file.length();
         splitSize = fileLength / emmitTaskRunnableNum;
-        // init emmit task output
-        for (int i = 0; i < emmitTaskRunnableNum; i++) {
-            emitTaskOutput.add(new HashMap<String, MutableInt>(50000, 09f));
-        }
+
         // init emmit task barrier
         emmitTaskBarrier = new CountDownLatch(emmitTaskRunnableNum);
+
+        //
+        //init
+        rankTaskRunnableNum = rn;
+        //
+        rankTaskOutput = new WCResult[rankTaskRunnableNum][];
+
+        // init emmit task output && rank task intput
+        for (int i = 0; i < emmitTaskRunnableNum; i++) {
+            ArrayList<HashMap<String, MutableInt>> rankSplits = new ArrayList<HashMap<String, MutableInt>>(emmitTaskRunnableNum);
+            for (int j = 0; j < rankTaskRunnableNum; j++) {
+                rankSplits.add(new HashMap<String, MutableInt>(10000, 0.9f)); // 4W / rankTaskRunnableNum
+            }
+            emitTaskOutput.add(rankSplits);
+        }
+        // emitTaskOutput.get(1-emmitTaskRunnableNum).get(id)
+    }
+
+    static void merge() {
+        ArrayList<WCResult> topTen = new ArrayList<WCResult>(16);
+        int index[] = new int[rankTaskRunnableNum];
+
+        for (int i = 0; i < 10; i++) {
+            WCResult max = null;
+            String top = null;
+            int maxIndex = -1;
+            for (int j = 0; j < rankTaskRunnableNum; j++) {
+                if (index[j] > 10) continue;
+                WCResult r = rankTaskOutput[j][index[j]];
+                if (max == null || max.cnt < r.cnt) {
+                    max = r;
+                    maxIndex = j;
+                }
+
+            }
+            index[maxIndex]++;
+            topTen.add(max);
+        }
+
+        for (WCResult word : topTen) {
+            System.out.println(word);
+        }
     }
 
     private static boolean isSplitter(byte c) {
@@ -77,7 +134,7 @@ public class WC10 {
     static class EmmitTaskRunnable implements Runnable {
         int id;
         int offset;
-        HashMap<String, MutableInt> output;
+        ArrayList<HashMap<String, MutableInt>> output;
 
         EmmitTaskRunnable(int id) {
             this.id = id;
@@ -136,8 +193,10 @@ public class WC10 {
                         // split word
                         String word = new String(buffer, 0, wordLen);
                         // word cnt
-                        MutableInt cnt = output.get(word);
-                        if (cnt == null) output.put(word, new MutableInt());
+                        int index = word.hashCode() % output.size();
+                        HashMap<String, MutableInt> rankSplit = output.get(index > 0 ? index : 0 - index);
+                        MutableInt cnt = rankSplit.get(word);
+                        if (cnt == null) rankSplit.put(word, new MutableInt());
                         else cnt.inc();
 
                         // reset
@@ -161,8 +220,10 @@ public class WC10 {
                         // split word
                         String word = new String(buffer, 0, wordLen);
                         // word cnt
-                        MutableInt cnt = output.get(word);
-                        if (cnt == null) output.put(word, new MutableInt());
+                        int index = word.hashCode() % output.size();
+                        HashMap<String, MutableInt> cntSplit = output.get(index > 0 ? index : 0 - index);
+                        MutableInt cnt = cntSplit.get(word);
+                        if (cnt == null) cntSplit.put(word, new MutableInt());
                         else cnt.inc();
                     }
                 }
@@ -174,11 +235,79 @@ public class WC10 {
         }
     }
 
+    static class RankTaskRunnable implements Runnable {
+        final int id;
+        final WCResult[] rank;
+
+        RankTaskRunnable(int id) {
+            this.id = id;
+            rank = new WCResult[10000];
+            rankTaskOutput[id] = rank;
+        }
+
+        public void run() {
+            int len = 0;
+            int top = 10;
+            for (int m = 0; m < emmitTaskRunnableNum; m++) {
+                HashMap<String, MutableInt> cntSplit = emitTaskOutput.get(m).get(id);
+                for (Map.Entry<String, MutableInt> entry : cntSplit.entrySet()) {
+                    int cnt = entry.getValue().value;
+                    String word = entry.getKey();
+
+                    if (cnt == 1) continue;
+
+                    if (!isStopWords(word)) {
+                        int i = 0;
+                        for (; i < len; i++) {
+                            if (rank[i] == null || cnt > rank[i].cnt) break;
+                        }
+
+                        if (len >= top && i == len) continue;
+                        else {
+                            if (rank[i] != null) {
+                                for (int j = len - 1; j >= i + 1; j--) {
+                                    rank[j] = rank[j - 1];
+                                }
+                            }
+                            rank[i] = new WCResult(word, cnt);
+                            len++;
+                        }
+                    }
+                }
+            }
+            rankTaskBarrier.countDown();
+
+        }
+    }
+    static boolean isStopWords(String word) {
+        if (word.length() > 6) {// stopword ÌØÕ÷
+            return false;
+        }
+        return stopWords.contains(word);
+    }
     static class MutableInt {
         int value = 1;
 
         void inc() {
             value++;
+        }
+    }
+
+    static class WCResult {
+        final String word;
+        final int cnt;
+
+        WCResult(String word, int cnt) {
+            this.word = word;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public String toString() {
+            return "WCResult{" +
+                    "word=" + word +
+                    ", cnt=" + cnt +
+                    '}';
         }
     }
 }
